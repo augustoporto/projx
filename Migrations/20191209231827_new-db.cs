3f2d3682e1b1115d2499538563b9de2fc4124206
augustoporto <augustoporto32@gmail.com>
2019-12-09 20:20:32 -0300

    Atualiza core

---
+++ Migrations/20191209231827_new-db.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace projx.Migrations
+{
+    public partial class newdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Ativa",
+                table: "Conta",
+                maxLength: 1,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldMaxLength: 1);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Conta",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Conta");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ativa",
+                table: "Conta",
+                type: "nvarchar(1)",
+                maxLength: 1,
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldMaxLength: 1);
+        }
+    }
+}
